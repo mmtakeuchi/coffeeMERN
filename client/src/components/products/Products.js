8eb0f4a6094a027b5d5263ea689c9675ef251346
mmtakeuchi <mikemtaki@gmail.com>
2021-06-29 12:30:52 -0700

    sepereated client and server files

---
+++ client/src/components/products/Products.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getProducts } from "../../actions/productActions";
+import Product from "./Product";
+import { Grid, Button } from "@material-ui/core";
+
+class Products extends React.Component {
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  renderProducts = () => {
+    const { products } = this.props;
+
+    if (products) {
+      if (products.length > 0) {
+        return products.map((product, i) => (
+          <Grid item key={i} xs={6} sm={4}>
+            <Product product={product} />
+          </Grid>
+        ));
+      }
+    }
+
+    return <div>No Products Found.</div>;
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div style={{marginBottom: '2.5em'}}>
+        <h1>Products</h1>
+        {this.props.current.user.isAdmin ? <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => this.props.history.push("/products/new")}
+          style={{ margin: "10px" }}
+        >
+          ADD NEW PRODUCT
+        </Button> : null}
+        
+
+        <Grid container spacing={4}>
+          {this.renderProducts()}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  products: state.products.products,
+  current: state.session
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getProducts: () => dispatch(getProducts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
