49c822944055e6338fcfcc81d5f246bd12ce2137
mmtakeuchi <mikemtaki@gmail.com>
2021-06-29 13:49:55 -0700

    pushing project to heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const db = require("./config/keys").mongoURI;
+const path = require("path");
 
 const userRoutes = require("./routes/users");
 const productRoutes = require("./routes/products");
@@ -22,13 +23,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(passport.initialize());
+
 require("./middleware/passport")(passport);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  stripeKey: process.env.STRIPE_KEY,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+};
